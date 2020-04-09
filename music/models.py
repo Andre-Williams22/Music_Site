81d04b8adeae3a1f3bd917e882c65d33d0adc70b
Andre Williams <awjrs22@gmail.com>
2020-04-09 12:46:25 -0700

    adds database migrations and models to application

--- music/models.py
+++ music/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Musician(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Song(models.Model):
+    artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    num_stars = models.IntegerField()
+
+    def __str__(self):
+        return self.name 
+class Album(models.Model):
+    name = models.CharField(max_length=100)
+
+    
+    def __str__(self):
+        return self.name 
--- music/views.py
+++ music/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse 
+
 
 # Create your views here.
+
+def about(request):
+    context = {
+        'favorite_color': 'blue'
+    }
+
+    return render(request, 'myapp/about.html', context)
+
